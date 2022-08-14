08c3859f447f1a9396b98edf9482092660d78c4b
James Go <jamescookgo@gmail.com>
2022-08-14 12:26:45 -0700

    Exit 1 on failure

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -2,5 +2,7 @@
 
 cd "$(dirname "$0")/.."
 
+rm dist/sqlite-migrator 2> /dev/null
+
 cargo build --target=x86_64-unknown-linux-gnu --release
 cp target/x86_64-unknown-linux-gnu/release/sqlite-migrator dist
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
 		Ok(_) => {}
 		Err(e) => {
 			eprintln!("Error: {}", e);
+			std::process::exit(1);
 		}
 	}
 }
