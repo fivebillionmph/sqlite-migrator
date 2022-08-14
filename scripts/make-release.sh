f75959dab2b246ed2f951e8ca3b7f770ff196875
James Go <jamescookgo@gmail.com>
2022-08-14 00:06:03 -0700

    Wrong filename

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -3,4 +3,4 @@
 cd "$(dirname "$0")/.."
 
 cargo build --target=x86_64-unknown-linux-gnu --release
-cp target/x86_64-unknown-linux-gnu/release/sqlite-manager dist
+cp target/x86_64-unknown-linux-gnu/release/sqlite-migrator dist
