328dff5ac8fb1df6050cc7aed47702c7a1457e81
James Go <jamescookgo@gmail.com>
2022-08-13 23:20:24 -0700

    Update CLI docs

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,7 @@
 #[derive(clap::Parser)]
+/// Reads sqlite files in a directory and attempts to migrate them using the PRAGMA user_version to determine which files should be run in the database.
+/// SQL files in the directory should follow the pattern m_{user_version}.sql
+/// For example, the following file names will run and be executed in order: m_001.sql, m_2.sql, m_03.sql
 pub struct Cli {
 	#[clap(subcommand)]
 	pub action: CliAction,
